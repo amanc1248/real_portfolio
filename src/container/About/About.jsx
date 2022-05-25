1dfad14164167060b4237b156a11971795aad14b
amanc1248 <amanc1248@gmail.com>
2022-05-25 15:46:42 +0545

    Add: Dynamic data from sanity

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-// import { urlFor, client } from '../../client';
+import { urlFor, client } from '../../client';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    // const query = '*[_type == "abouts"]';
+    const query = '*[_type == "abouts"]';
 
-    // client.fetch(query).then((data) => {
-    //   setAbouts(data);
-    // });
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
   }, []);
 
   return (
@@ -22,17 +21,17 @@ const About = () => {
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
-        {["abouts","hi"].map((about, index) => (
+        {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
-            key={"about.title + index"}
+            key={about.title + index}
           >
-            <img src={images.adidas} alt={"about.title"} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{"about.title"}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{"about.description"}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
